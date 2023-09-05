416b9e47281c6ba8584f2f8b171625ae47ce6058
Luka Koridze <lukakoridze13@gmail.com>
2023-09-05 22:02:47 +0400

    Delete Message

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ const MESSAGES = document.querySelector("#messages");
 
 // VARIABLES
 const COOLDOWN = 500;
+
 let MESSAGES_DATA = [
   {
+    id: 1,
     title: "Chen Lin",
     date: "14.02.2023",
     location: "Yuchi, Taiwan",
@@ -19,6 +21,7 @@ let MESSAGES_DATA = [
     isPinned: false,
   },
   {
+    id: 2,
     title: "Alice Johnson",
     date: "20.03.2023",
     location: "New York, USA",
@@ -29,6 +32,7 @@ let MESSAGES_DATA = [
     isPinned: false,
   },
   {
+    id: 3,
     title: "David Smith",
     date: "05.04.2023",
     location: "London, UK",
@@ -39,6 +43,7 @@ let MESSAGES_DATA = [
     isPinned: false,
   },
   {
+    id: 4,
     title: "Maria Garcia",
     date: "12.04.2023",
     location: "Madrid, Spain",
@@ -49,6 +54,7 @@ let MESSAGES_DATA = [
     isPinned: false,
   },
   {
+    id: 5,
     title: "Michael Brown",
     date: "25.04.2023",
     location: "Sydney, Australia",
@@ -59,6 +65,7 @@ let MESSAGES_DATA = [
     isPinned: true,
   },
   {
+    id: 6,
     title: "Sophie Wilson",
     date: "03.05.2023",
     location: "Toronto, Canada",
@@ -69,12 +76,17 @@ let MESSAGES_DATA = [
     isPinned: true,
   },
 ];
+
 let filteredData = MESSAGES_DATA;
 let BLACK_POSITION = "left";
 let ITTERATION = 0;
 let LAST_EXECUTION_TIME = 0;
 let xDown = null;
 let yDown = null;
+let expandFunction = null;
+let shrinkFunction = null;
+let animateAllowed = true;
+
 // ADDING STARTING LISTENERS
 PINNED.addEventListener("click", filter);
 window.addEventListener("wheel", handleWheelDown);
@@ -92,6 +104,233 @@ function handleWheelDown(event) {
     animateMessages();
     LAST_EXECUTION_TIME = currentTime;
   }
+}
+function animateMessages(starting) {
+  if (animateAllowed) {
+    let message;
+
+    document.querySelectorAll(".message").forEach((msg) => {
+      msg.removeEventListener("click", expandFunction);
+    });
+
+    if (starting === "start") {
+      MESSAGES.childNodes[0].classList.remove("invisible");
+      MESSAGES.childNodes[0].classList.add("goFirst");
+      if (filteredData.length > 1) {
+        MESSAGES.childNodes[1].classList.remove("invisible");
+        MESSAGES.childNodes[1].classList.add("goSecond");
+      }
+      if (filteredData.length > 2) {
+        MESSAGES.childNodes[2].classList.remove("invisible");
+        MESSAGES.childNodes[2].classList.add("goThird");
+      }
+      message = filteredData[0];
+    } else {
+      if (filteredData.length > 3) {
+        let current;
+        MESSAGES.childNodes.forEach((message, index) => {
+          if (message.classList.contains("goFirst")) {
+            current = index;
+          }
+          if (message.classList.contains("goDown")) {
+            message.classList.remove("goDown");
+            message.classList.add("invisible");
+          }
+        });
+
+        let first = current + 1;
+        if (current + 1 >= filteredData.length) {
+          first = 0;
+        }
+        let second = current + 2;
+        if (second >= filteredData.length) {
+          second = Math.abs(second - filteredData.length);
+        }
+        let third = current + 3;
+        if (third >= filteredData.length) {
+          third = Math.abs(third - filteredData.length);
+        }
+
+        MESSAGES.childNodes[current].classList.remove("goFirst");
+        MESSAGES.childNodes[current].classList.add("goDown");
+        MESSAGES.childNodes[first].classList.remove("goSecond");
+        MESSAGES.childNodes[first].classList.add("goFirst");
+
+        MESSAGES.childNodes[second].classList.remove("goThird");
+        MESSAGES.childNodes[second].classList.add("goSecond");
+        MESSAGES.childNodes[third].classList.remove("invisible");
+        MESSAGES.childNodes[third].classList.add("goThird");
+
+        message = filteredData[first];
+      } else if (filteredData.length === 3) {
+        let current;
+        MESSAGES.childNodes.forEach((message, index) => {
+          if (message.classList.contains("goFirst")) {
+            current = index;
+          }
+          if (message.classList.contains("goDown")) {
+            message.classList.remove("goDown");
+            message.classList.add("invisible");
+          }
+        });
+        let first = current + 1;
+        if (current + 1 >= filteredData.length) {
+          first = 0;
+        }
+        let second = current + 2;
+        if (second >= filteredData.length) {
+          second = Math.abs(second - filteredData.length);
+        }
+        MESSAGES.childNodes[current].classList.remove("goFirst");
+        MESSAGES.childNodes[current].classList.add("goThird");
+        MESSAGES.childNodes[first].classList.remove("goSecond");
+        MESSAGES.childNodes[first].classList.add("goFirst");
+
+        MESSAGES.childNodes[second].classList.remove("goThird");
+        MESSAGES.childNodes[second].classList.add("goSecond");
+
+        message = filteredData[first];
+      } else if (filteredData.length === 2) {
+        let current;
+        MESSAGES.childNodes.forEach((message, index) => {
+          if (message.classList.contains("goFirst")) {
+            current = index;
+          }
+          if (message.classList.contains("goDown")) {
+            message.classList.remove("goDown");
+            message.classList.add("invisible");
+          }
+        });
+
+        let first = current + 1;
+        if (current + 1 >= filteredData.length) {
+          first = 0;
+        }
+
+        MESSAGES.childNodes[current].classList.remove("goFirst");
+        MESSAGES.childNodes[current].classList.add("goSecond");
+        MESSAGES.childNodes[first].classList.remove("goSecond");
+        MESSAGES.childNodes[first].classList.add("goFirst");
+
+        message = filteredData[first];
+      } else {
+        message = filteredData[0];
+      }
+    }
+
+    expandFunction = () => {
+      expand(document.querySelector(".goFirst"), message, expandFunction);
+    };
+    setTimeout(
+      document
+        .querySelector(".goFirst")
+        .addEventListener("click", expandFunction),
+      300
+    );
+  }
+}
+function expand(messageDiv, message) {
+  console.log(messageDiv);
+  animateAllowed = false;
+  messageDiv.style.transition = "all 0.3s";
+  messageDiv.id = "expand";
+  MESSAGES.style.position = "static";
+  document.querySelector("#expand p").textContent = message.long_Text;
+  document.querySelector("#expand .buttons").style.display = "flex";
+  messageDiv.removeEventListener("click", expandFunction);
+  shrinkFunction = () => {
+    shrink(messageDiv, message);
+  };
+  messageDiv.addEventListener("click", shrinkFunction);
+}
+function shrink(messageDiv, message) {
+  animateAllowed = true;
+  messageDiv.removeEventListener("click", shrinkFunction);
+  document.querySelector("#expand p").textContent = message.short_Text;
+  document.querySelector("#expand .buttons").style.display = "none";
+  messageDiv.removeAttribute("id");
+  MESSAGES.style.position = "relative";
+  setTimeout(() => {
+    messageDiv.style.transition = "all 1s";
+    messageDiv.addEventListener("click", expandFunction);
+  }, 300);
+}
+function renderMessages() {
+  drawMessages(filteredData);
+
+  if (filteredData.length > 0) {
+    animateMessages("start");
+  }
+}
+function drawMessages(array) {
+  MESSAGES.innerHTML = "";
+  if (array.length === 0) {
+    MESSAGES.innerHTML = "No Messages!";
+  } else {
+    array.forEach((message, index) => drawMessage(message, index));
+  }
+}
+function drawMessage(message, index) {
+  const messageDiv = document.createElement("div");
+  messageDiv.className = "message";
+
+  if (index > 2) {
+    messageDiv.classList.add("invisible");
+  }
+
+  const titleElement = document.createElement("h1");
+  titleElement.textContent = message.title;
+
+  const dateLocationElement = document.createElement("span");
+  dateLocationElement.textContent = `${message.date} ${message.location}`;
+
+  const imageElement = document.createElement("img");
+  imageElement.src = message.image;
+  imageElement.alt = "";
+
+  const shortTextElement = document.createElement("p");
+  shortTextElement.textContent = message.short_Text;
+
+  const buttonsDiv = document.createElement("div");
+  buttonsDiv.className = "buttons";
+
+  const trashButtonDiv = document.createElement("div");
+  trashButtonDiv.className = "trash";
+  const trashButtonSpan = document.createElement("span");
+  trashButtonSpan.textContent = "Trash";
+  trashButtonDiv.appendChild(trashButtonSpan);
+
+  trashButtonDiv.addEventListener("click", (event) => {
+    event.stopPropagation();
+    deleteMessage(message.id);
+    shrink(messageDiv, message);
+    renderMessages();
+  });
+
+  const middleeDiv = document.createElement("div");
+  middleeDiv.className = "middlee";
+
+  const replyButtonDiv = document.createElement("div");
+  replyButtonDiv.className = "reply";
+  const replyButtonSpan = document.createElement("span");
+  replyButtonSpan.textContent = "Reply";
+  replyButtonDiv.appendChild(replyButtonSpan);
+
+  messageDiv.appendChild(titleElement);
+  messageDiv.appendChild(dateLocationElement);
+  messageDiv.appendChild(imageElement);
+  messageDiv.appendChild(shortTextElement);
+  messageDiv.appendChild(buttonsDiv);
+  buttonsDiv.appendChild(trashButtonDiv);
+  buttonsDiv.appendChild(middleeDiv);
+  buttonsDiv.appendChild(replyButtonDiv);
+
+  MESSAGES.appendChild(messageDiv);
+}
+function deleteMessage(id) {
+  MESSAGES_DATA = MESSAGES_DATA.filter((message) => message.id !== id);
+  filteredData = MESSAGES_DATA;
+  updateCount();
 }
 function filter() {
   if (BLACK_POSITION === "left") {
@@ -117,64 +356,6 @@ function filter() {
     BLACK_POSITION = "left";
   }
 }
-function animateReverse() {
-  BLACK.style.left = "50%";
-  BLACK.style.height = "16px";
-  BLACK.style.width = "20%";
-  BLACK.style.borderTopLeftRadius = "100%";
-  BLACK.style.borderBottomLeftRadius = "100%";
-  BLACK.style.borderTopRightRadius = "20px";
-  BLACK.style.borderBottomRightRadius = "20px";
-  setTimeout(() => {
-    BLACK.style.left = "2%";
-    BLACK.style.height = "32px";
-    BLACK.style.width = "44%";
-    BLACK.style.borderRadius = "20px";
-  }, 120);
-}
-function animate() {
-  BLACK.style.borderTopRightRadius = "100%";
-  BLACK.style.borderBottomRightRadius = "100%";
-  BLACK.style.borderTopLeftRadius = "20px";
-  BLACK.style.borderBottomLeftRadius = "20px";
-  BLACK.style.left = "50%";
-  BLACK.style.height = "16px";
-  BLACK.style.width = "20%";
-  setTimeout(() => {
-    BLACK.style.borderTopLeftRadius = "100%";
-    BLACK.style.borderBottomLeftRadius = "100%";
-    BLACK.style.borderTopRightRadius = "20px";
-    BLACK.style.borderBottomRightRadius = "20px";
-    BLACK.style.left = "54%";
-    BLACK.style.height = "32px";
-    BLACK.style.width = "44%";
-    BLACK.style.borderRadius = "20px";
-  }, 120);
-}
-function drawMessages(array) {
-  MESSAGES.innerHTML = "";
-  array.forEach((message, index) => drawMessage(message, index));
-}
-function drawMessage(message, index) {
-  const messageDiv = document.createElement("div");
-
-  messageDiv.className = "message";
-  if (index > 2) {
-    messageDiv.classList.add("invisible");
-  }
-  messageDiv.innerHTML = `
-  <h1>${message.title}</h1>
-  <span>${message.date} ${message.location}</span>
-  <img src="${message.image}" alt="" />
-  <p>${message.short_Text}</p>
-  <div class="buttons">
-    <div class="trash"><span>Trash</span></div>
-    <div class="middlee"></div>
-    <div class="reply"><span>Reply</span></div>
-  </div>
-  `;
-  MESSAGES.appendChild(messageDiv);
-}
 function shiftArrayLeft(arr) {
   if (arr.length <= 1) {
     return arr;
@@ -183,147 +364,17 @@ function shiftArrayLeft(arr) {
   arr.push(firstElement);
   return arr;
 }
-function animateMessages(starting) {
-  if (starting === "start") {
-    MESSAGES.childNodes[0].classList.remove("invisible");
-    MESSAGES.childNodes[0].classList.add("goFirst");
-    MESSAGES.childNodes[0].addEventListener("click", () => {
-      expand(MESSAGES.childNodes[0], filteredData[0]);
-    });
-    if (filteredData.length > 1) {
-      MESSAGES.childNodes[1].classList.remove("invisible");
-      MESSAGES.childNodes[1].classList.add("goSecond");
-    }
-    if (filteredData.length > 2) {
-      MESSAGES.childNodes[2].classList.remove("invisible");
-      MESSAGES.childNodes[2].classList.add("goThird");
-    }
-  } else {
-    if (filteredData.length > 3) {
-      let current;
-      MESSAGES.childNodes.forEach((message, index) => {
-        if (message.classList.contains("goFirst")) {
-          current = index;
-        }
-        if (message.classList.contains("goDown")) {
-          message.classList.remove("goDown");
-          message.classList.add("invisible");
-        }
-      });
-
-      let first = current + 1;
-      if (current + 1 >= filteredData.length) {
-        first = 0;
-      }
-      let second = current + 2;
-      if (second >= filteredData.length) {
-        second = Math.abs(second - filteredData.length);
-      }
-      let third = current + 3;
-      if (third >= filteredData.length) {
-        third = Math.abs(third - filteredData.length);
-      }
-
-      MESSAGES.childNodes[current].classList.remove("goFirst");
-      MESSAGES.childNodes[current].classList.add("goDown");
-      MESSAGES.childNodes[first].classList.remove("goSecond");
-      MESSAGES.childNodes[first].classList.add("goFirst");
-      MESSAGES.childNodes[first].addEventListener("click", () => {
-        expand(MESSAGES.childNodes[first], filteredData[first]);
-      });
-      MESSAGES.childNodes[second].classList.remove("goThird");
-      MESSAGES.childNodes[second].classList.add("goSecond");
-      MESSAGES.childNodes[third].classList.remove("invisible");
-      MESSAGES.childNodes[third].classList.add("goThird");
-    } else if (filteredData.length === 3) {
-      let current;
-      MESSAGES.childNodes.forEach((message, index) => {
-        if (message.classList.contains("goFirst")) {
-          current = index;
-        }
-        if (message.classList.contains("goDown")) {
-          message.classList.remove("goDown");
-          message.classList.add("invisible");
-        }
-      });
-
-      let first = current + 1;
-      if (current + 1 >= filteredData.length) {
-        first = 0;
-      }
-      let second = current + 2;
-      if (second >= filteredData.length) {
-        second = Math.abs(second - filteredData.length);
-      }
-
-      MESSAGES.childNodes[current].classList.remove("goFirst");
-      MESSAGES.childNodes[current].classList.add("goThird");
-      MESSAGES.childNodes[first].classList.remove("goSecond");
-      MESSAGES.childNodes[first].classList.add("goFirst");
-      MESSAGES.childNodes[first].addEventListener("click", () => {
-        expand(MESSAGES.childNodes[first], filteredData[first]);
-      });
-      MESSAGES.childNodes[second].classList.remove("goThird");
-      MESSAGES.childNodes[second].classList.add("goSecond");
-    } else if (filteredData.length === 2) {
-      let current;
-      MESSAGES.childNodes.forEach((message, index) => {
-        if (message.classList.contains("goFirst")) {
-          current = index;
-        }
-        if (message.classList.contains("goDown")) {
-          message.classList.remove("goDown");
-          message.classList.add("invisible");
-        }
-      });
-
-      let first = current + 1;
-      if (current + 1 >= filteredData.length) {
-        first = 0;
-      }
-
-      MESSAGES.childNodes[current].classList.remove("goFirst");
-      MESSAGES.childNodes[current].classList.add("goSecond");
-      MESSAGES.childNodes[first].classList.remove("goSecond");
-      MESSAGES.childNodes[first].classList.add("goFirst");
-      MESSAGES.childNodes[first].addEventListener("click", () => {
-        expand(MESSAGES.childNodes[first], filteredData[first]);
-      });
-    }
-  }
-}
-
-function expand(messageDiv, message) {
-  if (messageDiv.id !== "expand") {
-    messageDiv.style.transition = "all 0.3s";
-    messageDiv.id = "expand";
-    MESSAGES.style.position = "static";
-    document.querySelector("#expand p").textContent = message.long_Text;
-    document.querySelector("#expand .buttons").style.display = "flex";
-  } else {
-    document.querySelector("#expand p").textContent = message.short_Text;
-    document.querySelector("#expand .buttons").style.display = "none";
-    messageDiv.setAttribute("id", "");
-    MESSAGES.style.position = "relative";
-    setTimeout(() => {
-      messageDiv.style.transition = "all 1s";
-    }, 300);
-  }
-}
-
 function getTouches(evt) {
   return (
     evt.touches || // browser API
     evt.originalEvent.touches
   ); // jQuery
 }
-
 function handleTouchStart(evt) {
   const firstTouch = getTouches(evt)[0];
   xDown = firstTouch.clientX;
   yDown = firstTouch.clientY;
 }
-
 function handleTouchMove(evt) {
   if (!xDown || !yDown) {
     return;
@@ -355,4 +406,41 @@ function handleTouchMove(evt) {
   }
   xDown = null;
   yDown = null;
+}
+function animate() {
+  BLACK.style.borderTopRightRadius = "100%";
+  BLACK.style.borderBottomRightRadius = "100%";
+  BLACK.style.borderTopLeftRadius = "20px";
+  BLACK.style.borderBottomLeftRadius = "20px";
+  BLACK.style.left = "50%";
+  BLACK.style.height = "16px";
+  BLACK.style.width = "20%";
+  setTimeout(() => {
+    BLACK.style.borderTopLeftRadius = "100%";
+    BLACK.style.borderBottomLeftRadius = "100%";
+    BLACK.style.borderTopRightRadius = "20px";
+    BLACK.style.borderBottomRightRadius = "20px";
+    BLACK.style.left = "54%";
+    BLACK.style.height = "32px";
+    BLACK.style.width = "44%";
+    BLACK.style.borderRadius = "20px";
+  }, 120);
+}
+function animateReverse() {
+  BLACK.style.left = "50%";
+  BLACK.style.height = "16px";
+  BLACK.style.width = "20%";
+  BLACK.style.borderTopLeftRadius = "100%";
+  BLACK.style.borderBottomLeftRadius = "100%";
+  BLACK.style.borderTopRightRadius = "20px";
+  BLACK.style.borderBottomRightRadius = "20px";
+  setTimeout(() => {
+    BLACK.style.left = "2%";
+    BLACK.style.height = "32px";
+    BLACK.style.width = "44%";
+    BLACK.style.borderRadius = "20px";
+  }, 120);
+}
+function updateCount() {
+  COUNT.textContent = MESSAGES_DATA.length;
 }
